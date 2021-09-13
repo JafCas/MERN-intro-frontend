871351523c63222e7d5615b397b8abaa6eaa9467
Jafet Castillo <jafetkaid@gmail.com>
2021-09-12 19:14:29 -0500

    Added GET, POST and DELETE functionality to notes

---
+++ src/components/CreateNote.js
@@ -0,0 +1,101 @@
+import React, { Component } from "react";
+import axios from "axios";
+import DatePicker from "react-datepicker";
+import "react-datepicker/dist/react-datepicker.css";
+
+class CreateNote extends Component {
+  state = {
+    users: [],
+    userSelected: "",
+    title: "",
+    content: "",
+    date: new Date(),
+  };
+
+  async componentDidMount() {
+    const res = await axios.get("http://localhost:4000/api/users");
+    this.setState({
+      users: res.data.map((user) => user.username),
+      userSelected: res.data[0].username,
+    });
+  }
+
+  onSubmit = async (e) => {
+    e.preventDefault();
+    const newNote = {
+      title: this.state.title,
+      content: this.state.content,
+      date: this.state.date,
+      author: this.state.userSelected,
+    };
+    await axios.post("http://localhost:4000/api/notes/", newNote);
+    window.location.href = "/";
+  };
+
+  onInputChange = async (e) => {
+    this.setState({
+      [e.target.name]: e.target.value,
+    });
+  };
+
+  onChangeDate = (date) => {
+    this.setState({ date });
+  };
+
+  render() {
+    return (
+      <div className="col-md-6 offset-md-3">
+        <div className="card card-body">
+          <h4>Create a note</h4>
+          {/* SELECT USER */}
+          <div className="form-grou">
+            <select
+              className="form-control"
+              name="useSelected"
+              onChange={this.onInputChange}
+            >
+              {this.state.users.map((user) => (
+                <option key={user} value={user}>
+                  {user}
+                </option>
+              ))}
+            </select>
+          </div>
+          <div className="form-group">
+            <input
+              type="text"
+              className="form-control"
+              placeholder="Title"
+              name="title"
+              required
+              onChange={this.onInputChange}
+            />
+          </div>
+          <div className="form-group">
+            <textarea
+              name="content"
+              className="form-control"
+              placeholder="content"
+              required
+              onChange={this.onInputChange}
+            ></textarea>
+          </div>
+          <div className="form-group">
+            <DatePicker
+              className="form-control"
+              selected={this.state.date}
+              onChange={this.onChangeDate}
+            />
+          </div>
+          <form onSubmit={this.onSubmit}>
+            <button type="submit" className="btn btn-primary">
+              Save note
+            </button>
+          </form>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default CreateNote;
